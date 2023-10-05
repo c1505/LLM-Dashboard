7d69bdabd37f6f7b36e224759584ebe35216978b
Corey Morris <coreym247@gmail.com>
2023-10-05 16:30:52 -0400

    moved organization column to the front

--- result_data_processor.py
+++ result_data_processor.py
@@ -140,6 +140,9 @@ class ResultDataProcessor:
         cols = cols[:2] + cols[-1:] + cols[2:-1]
         data = data[cols]
 
+
+
+
         # Drop specific columns
         data = data.drop(columns=['all', 'truthfulqa:mc|0'])
 
@@ -147,6 +150,12 @@ class ResultDataProcessor:
         data['Parameters'] = data.index.to_series().apply(self._extract_parameters)
 
         # move the parameters column to the front of the dataframe
+        cols = data.columns.tolist()
+        cols = cols[-1:] + cols[:-1]
+        print(cols)
+        data = data[cols]
+
+        # Reorder columns to move 'organization' to the second position
         cols = data.columns.tolist()
         cols = cols[-1:] + cols[:-1]
         data = data[cols]
